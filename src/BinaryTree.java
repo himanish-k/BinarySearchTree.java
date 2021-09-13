9a6c83ac1b10f6fa3d1fc981e39725d43aa10309
himanish <himanish.kaushal@gmail.com>
2021-09-13 18:34:55 -0400

    add and isExist functionality without recursion

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -3,7 +3,54 @@ public class BinaryTree {
     Node root;
 
     public void add(int value) {
-        return;
+        if (root == null)
+            root = new Node(value);
+
+        Node curr = root;
+        Node newNode = new Node(value);
+        while (curr != null) {
+            if (value <= curr.getValue()) {
+                if (curr.getLeftChild() == null) {
+                    curr.setLeftChild(newNode);
+                    return;
+                } else {
+                    curr = curr.getLeftChild();
+                }
+            } else {
+                if (curr.getRightChild() == null) {
+                    curr.setRightChild(newNode);
+                    return;
+                } else {
+                    curr = curr.getRightChild();
+                }
+            }
+        }
+    }
+
+    public boolean isExist(int value) {
+        if (root == null)
+            return false;
+
+        Node curr = root;
+        while (curr != null) {
+            if (curr.getValue() == value) {
+                return true;
+            } else if (value < curr.getValue()) {
+                if (curr.getLeftChild() == null) {
+                    return false;
+                } else {
+                    curr = curr.getLeftChild();
+                }
+            } else {
+                if (curr.getRightChild() == null) {
+                    return false;
+                } else {
+                    curr = curr.getRightChild();
+                }
+            }
+        }
+
+        return false;
     }
 
     public boolean remove(int value) {
@@ -11,15 +58,27 @@ public class BinaryTree {
     }
 
     class Node {
-        int value;
-        Node leftChild, rightChild;
+        private int value;
+        private Node leftChild, rightChild;
 
         public Node(int value) {
             this.value = value;
         }
 
+        public int getValue() {
+            return value;
+        }
+
+        public Node getLeftChild() {
+            return this.leftChild;
+        }
+
         public void setLeftChild(Node leftChild) {
             this.leftChild = leftChild;
+        }
+
+        public Node getRightChild() {
+            return this.rightChild;
         }
 
         public void setRightChild(Node rightChild) {
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        BinaryTree tree = new BinaryTree();
+        tree.add(1);
+        tree.add(2);
+        tree.add(3);
+        System.out.println("Does 1 exist? " + tree.isExist(1));
+        System.out.println("Does 2 exist? " + tree.isExist(2));
+        System.out.println("Does 3 exist? " + tree.isExist(3));
+        System.out.println("Does 4 exist? " + tree.isExist(4));
     }
 }
